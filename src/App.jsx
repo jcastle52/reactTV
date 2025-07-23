a44cfbee9e92868c02f37bf1e6087a6a6cad4a17
Jcastle52 <castillojasiel11@gmail.com>
2025-07-22 21:12:52 -0500

    finished nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+//import { useState } from "react";
+import { tvShows } from "./shows/data";
+import ShowSelection from "./shows/ShowSelection";
+//import EpisodeList from "./episodes/EpisodeList"
+
 export default function App() {
+  //const [selectedEpisode, setSelectedEpisode] = useState();
+
   return (
     <>
       <header>
         <p>React TV</p>
+        {ShowSelection(tvShows)}
       </header>
-      <main></main>
+      <main>
+
+      </main>
     </>
   );
 }
--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,18 @@
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection(tvShows) {
+  return (
+    <>
+      <nav className="shows">
+        {tvShows.map((element, index) => (
+          <a className="show" key={index}
+          onClick={() => (
+            console.log("test")
+          )}
+          >{element.name}</a>
+        ))}
+      </nav>
+    </>
+  );
 }
